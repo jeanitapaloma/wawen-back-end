6bd06ee71448a9e7710f6cf53c984af664c4f8b4
capstone student <capstonestudent@pn.local>
2020-10-06 21:33:11 +0800

    wala ra

--- routes/api.php
+++ routes/api.php
@@ -21,13 +21,12 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/login', 'AuthController@login');
-
-
 Route::post('/register','AuthController@register');
-
 Route::resource('categories', 'CategoryController');
 Route::resource('products', 'ProductController');
 Route::resource('orders', 'OrderController');
 
 
-Route::get('/dummy', 'DummyController@test');
+Route::get('/test', function () {
+    return response(User::all());
+});
